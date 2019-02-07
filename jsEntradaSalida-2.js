63fa9c9cf116120792222568cc1a0187b5d61ca0
JulietaCaro <47424416+JulietaCaro@users.noreply.github.com>
2019-02-07 16:12:05 -0300

    ejercicios entrada/salida
    del 1 al 6

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,8 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	var person;
+	person=prompt("Please enter your name", "Harry Potter");
+}
+
---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,13 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+mostrar el resulto por medio de "ALERT"*/
+function sumar()
+{
+	var numero1
+	var numero2
+	var suma
+
+	numero1=numeroUno.value;
+	numero2=numeroDos.value;
+}
+
