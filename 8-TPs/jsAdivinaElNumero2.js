31dd8223622166ce2b93a7d9d07212fbf4129588
JulietaCaro <julietacaro1d@gmail.com>
2019-02-22 10:12:45 -0300

    parcial

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -15,14 +15,54 @@ var contadorIntentos;
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
-	
+	numeroSecreto=Math.floor(Math.random() * 100) + 1;
 
+	console.log("numero secreto:"+numeroSecreto);
+
+	contadorIntentos=0 
 }
 
 function verificar()
 {
-	
-	
+	var numeroIngresado;
+	var mensaje;
 
+	contadorIntentos=contadorIntentos+1;
+	contadorIntentos=parseInt(contadorIntentos);
+
+	numeroIngresado=numero.value;
+	console.log(numeroIngresado);
+
+
+	switch(contadorIntentos)
+		{	
+			caso 1:
+				mensaje="Usted es Psíquico";
+				break,
+			case 2:
+				mensaje="Excelente percepción";
+				break;
+			case 3:
+				mensaje="Esto es suerte";
+				break;
+			case 4:
+				mensaje="Excelente tecnica";
+				break;
+			case 5:
+				mensaje="Usted está en la media";
+				break;
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+				mensaje="falta tecnica";
+				break;
+
+			default:
+				mensaje="Afortunado en el amor!!";
+				break;
+		}
+
+	alert(mensaje);
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,100 @@
 function mostrar()
 {
+	
+	var primerImporte;
+	var segundoImporte;
+	var tercerImporte;
+	var cuartoImporte;
+	var total;
+	var totalConDescuento;
+	var mayor;
+
+	primerImporte=prompt("Diga el primer importe");
+	segundoImporte=prompt("Diga el segundo importe");
+	tercerImporte=prompt("Diga el tercer importe");
+	cuartoImporte=prompt("Diga el cuarto importe");
+
+
+	primerImporte=parseInt(primerImporte);
+	segundoImporte=parseInt(segundoImporte);
+	tercerImporte=parseInt(tercerImporte);
+	cuartoImporte=parseInt(cuartoImporte);
+
+	
+	total=primerImporte+segundoImporte+tercerImporte+cuartoImporte;
+
+
+	
+
+	if(total>100)
+	{
+		totalConDescuento=total*0.9;
+
+	}
+	else
+	{
+		if(total>50)
+		{
+			totalConDescuento=total*0.95;
+
+		}	
+
+	
+		else
+		{
+		totalConDescuento=total*1.15;
+
+		}
+
+	}	
+
+alert(totalConDescuento);
+
+	primerImporte=document.getElementById('importePrimero').value;
+	segundoImporte=document.getElementById("importeSegundo").value;
+	tercerImporte=document.getElementById("importeTercero").value;
+	cuartoImporte=document.getElementById("cuartoImporte").value;
+	
+
+	if(primerImporte>=segundoImporte && primerImporte>=tercerImporte && primerImporte>=cuartoImporte)
+	{
+		mayor=primerImporte;
+	}
+	else
+	{
+		if(segundoImporte>=tercerImporte && primerImporte>=cuartoImporte)
+		{	
+			mayor=segundoImporte;
+		}
+		else
+		{
+			if(tercerImporte>=cuartoImporte)
+			{
+				mayor=tercerImporte;
+			}
+			else
+			{
+				mayor=cuartoImporte;
+			}
+
+		}
+
+	}
+	
+	alert(mayor);	
+
+	
+
+
+
+
+
+
+
+
+
+
+
+	
 
 }
